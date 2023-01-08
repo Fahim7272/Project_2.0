f511dfccc1ed6c7679005c71d5e543a822d30ff2
Fahim Ahmed Patwary <www.fahimahmed2727@gmail.com>
2023-01-08 15:59:57 +0600

    Student Login

---
+++ src/main/java/com/example/demo1/MainApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class MainApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("login1.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 765, 506);
+        stage.initStyle(StageStyle.DECORATED.UNDECORATED);
+        //stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
