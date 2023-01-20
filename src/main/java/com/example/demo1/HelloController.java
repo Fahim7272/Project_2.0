16e4b8d9015ab41a98d0b6192617ab9053b8b877
Fahim Ahmed Patwary <www.fahimahmed2727@gmail.com>
2023-01-20 15:31:52 +0600

    Student Login

--- src/main/java/com/example/demo1/HelloController.java
+++ src/main/java/com/example/demo1/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.demo1;
 
+import com.sun.jdi.connect.spi.Connection;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.event.ActionEvent;
@@ -7,6 +8,11 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ResourceBundle;
 
 public class HelloController {
 
@@ -19,10 +25,23 @@ public class HelloController {
         @FXML
         private PasswordField password;
 
+        private Connection connect;
+        private Statement statement;
+        private PreparedStatement preparedStatement;
+        private ResultSet resultSet;
+
+
         @FXML
         void exit(ActionEvent event) {
                 System.exit(0);
         }
+
+        public void login(){
+                connect = DB.connectDb();
+
+                String sql;
+        }
+
 
 }
 
