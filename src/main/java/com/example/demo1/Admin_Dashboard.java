97dd28e92b8931426f312c006fa2de92f146aeb5
Fahim Ahmed Patwary <www.fahimahmed2727@gmail.com>
2023-01-22 23:42:08 +0600

    Student Login

--- src/main/java/com/example/demo1/Admin_Dashboard.java
+++ src/main/java/com/example/demo1/Admin_Dashboard.java
@@ -1,6 +1,60 @@
 package com.example.demo1;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class Admin_Dashboard {
 
+    @FXML
+    void Classrooms(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Contact_info_insert(ActionEvent event) {
+
+    }
+
+    @FXML
+    void General_info_insert(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Post_announce(ActionEvent event) {
+
+    }
+
+    @FXML
+    void exit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void help_chat(ActionEvent event) {
+
+    }
+
+    @FXML
+    void home(ActionEvent event) {
+
+    }
+
+    @FXML
+    void log_out(ActionEvent event) {
+
+    }
+
+    @FXML
+    void new_student_data(ActionEvent event) {
+
+    }
+
+    @FXML
+    void result_update(ActionEvent event) {
+
+    }
 
 }
