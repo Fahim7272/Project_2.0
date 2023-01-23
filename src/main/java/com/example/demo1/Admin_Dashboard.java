496c301f53249c37d6452359d7641279f155660b
Fahim Ahmed Patwary <www.fahimahmed2727@gmail.com>
2023-01-24 02:22:31 +0600

    Student Login

--- src/main/java/com/example/demo1/Admin_Dashboard.java
+++ src/main/java/com/example/demo1/Admin_Dashboard.java
@@ -29,7 +29,37 @@ public class Admin_Dashboard {
     private Button General_info_insert;
 
     @FXML
+    private AnchorPane General_info_pane;
+
+    @FXML
+    private Button Next_page_result;
+
+    @FXML
     private Button Post_announce;
+
+    @FXML
+    private AnchorPane announce_pane;
+
+    @FXML
+    private Button back_to_new_info;
+
+    @FXML
+    private TextField birth_date_in;
+
+    @FXML
+    private TextField blood_grp_in;
+
+    @FXML
+    private AnchorPane contact_info_pane;
+
+    @FXML
+    private TextField fathers_name;
+
+    @FXML
+    private TextField first_name;
+
+    @FXML
+    private TextField gender_in;
 
     @FXML
     private Button help_chat;
@@ -41,10 +71,16 @@ public class Admin_Dashboard {
     private AnchorPane home_pane;
 
     @FXML
-    private AnchorPane General_info_pane;
+    private Button jump_to_contact;
+
+    @FXML
+    private TextField last_name;
 
     @FXML
     private Button log_out;
+
+    @FXML
+    private TextField mothers_name;
 
     @FXML
     private Button new_student_data;
@@ -53,16 +89,31 @@ public class Admin_Dashboard {
     private AnchorPane new_student_pane;
 
     @FXML
-    private AnchorPane announce_pane;
+    private Button post_announce_btn;
+
+    @FXML
+    private TextField post_writer;
+
+    @FXML
+    private TextField religion_in;
 
     @FXML
     private Button result_update;
 
     @FXML
-    private Button post_announce_btn;
+    private AnchorPane result_update_pane;
 
     @FXML
-    private TextField post_writer;
+    private AnchorPane result_update_pane1;
+
+    @FXML
+    private AnchorPane result_update_pane2;
+
+    @FXML
+    private Button save_new_data;
+
+    @FXML
+    private TextField std_ID_input;
 
     @FXML
     void exit(ActionEvent event) {
@@ -76,15 +127,41 @@ public class Admin_Dashboard {
             new_student_pane.setVisible(false);
             General_info_pane.setVisible(false);
             announce_pane.setVisible(false);
+            result_update_pane.setVisible(false);
+            result_update_pane1.setVisible(false);
+            result_update_pane2.setVisible(false);
         }
         else if(event.getSource() == new_student_data){
             new_student_pane.setVisible(true);
             General_info_pane.setVisible(true);
             home_pane.setVisible(false);
             announce_pane.setVisible(false);
+            result_update_pane.setVisible(false);
+            result_update_pane1.setVisible(false);
+            result_update_pane2.setVisible(false);
         }
         else if(event.getSource() == Post_announce){
             announce_pane.setVisible(true);
+            new_student_pane.setVisible(false);
+            General_info_pane.setVisible(false);
+            home_pane.setVisible(false);
+            result_update_pane.setVisible(false);
+            result_update_pane1.setVisible(false);
+            result_update_pane2.setVisible(false);
+        }
+        else if(event.getSource() == result_update){
+            result_update_pane.setVisible(true);
+            result_update_pane1.setVisible(true);
+            announce_pane.setVisible(false);
+            new_student_pane.setVisible(false);
+            General_info_pane.setVisible(false);
+            home_pane.setVisible(false);
+        }
+        else if(event.getSource() == Next_page_result){
+            result_update_pane.setVisible(true);
+            result_update_pane1.setVisible(false);
+            result_update_pane2.setVisible(true);
+            announce_pane.setVisible(false);
             new_student_pane.setVisible(false);
             General_info_pane.setVisible(false);
             home_pane.setVisible(false);
