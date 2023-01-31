51b0c904dd4736ff12342bdeef069ae3acb967fc
Fahim Ahmed Patwary <www.fahimahmed2727@gmail.com>
2023-01-31 21:46:07 +0600

    Database Update

---
+++ src/main/java/com/example/demo1/Teachers_Dashboard.java
@@ -0,0 +1,45 @@
+package com.example.demo1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+
+public class Teachers_Dashboard {
+
+    @FXML
+    private Button Help_chat;
+
+    @FXML
+    private ChoiceBox<?> Upd_res_class;
+
+    @FXML
+    private Button Upd_res_proceed;
+
+    @FXML
+    private ChoiceBox<?> Upd_res_section;
+
+    @FXML
+    private ChoiceBox<?> announcement_class;
+
+    @FXML
+    private Button announcement_proceed;
+
+    @FXML
+    private ChoiceBox<?> announcement_section;
+
+    @FXML
+    private Button log_out_teachers;
+
+    @FXML
+    private Button post_announce;
+
+    @FXML
+    private Button result_update;
+
+    @FXML
+    private Button teachers_home;
+
+    @FXML
+    private Button teachers_routine;
+
+}
